0e12e9b1c406e068065a3fcf9908936a4db4e5a8
jihyeon <20181039@sungshin.ac.kr>
2021-01-29 22:09:54 +0900

    결합도 낮추기 - Factory 패턴

--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -3,7 +3,8 @@ package polymorphism;
 public class TVUser {
 
 	public static void main(String[] args) {
-		TV tv = new SamsungTV();
+		BeanFactory factory = new BeanFactory();
+		TV tv = (TV) factory.getBean(args[0]);
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
