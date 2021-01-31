4045a1b82adfb69bb1fe6866e4d92fc8f3a07c7c
jihyeon <20181039@sungshin.ac.kr>
2021-01-31 23:10:15 +0900

    결합도 낮추기 - 스프링 컨테이너

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -2,6 +2,10 @@ package polymorphism;
 
 public class SamsungTV implements TV{
 	
+	public SamsungTV() {
+		System.out.println("===> SamsungTV) 객체 생성");
+	}
+	
 	@Override
 	public void powerOn() {
 		System.out.println("SamsungTV---전원 켠다.");	
--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -1,14 +1,27 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 
 	public static void main(String[] args) {
-		BeanFactory factory = new BeanFactory();
-		TV tv = (TV) factory.getBean(args[0]);
+		/*
+		 1. Spring 컨테이너를 구동한다.
+		   =>  환경설정 파일인 applicationContext.xml을 로딩하여 스프링 컨테이너 중 하나인 CenericXmlApplicationContext를 구동시킴
+		 */
+		
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		// 2. Spring 컨테이너로부터 필요한 객체를 요청(Lookup) 한다.
+		TV tv = (TV)factory.getBean("tv");
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
+		
+		// 3. Spring 컨테이너를 종료한다.
+		factory.close();
 	}
-
+	
 }
