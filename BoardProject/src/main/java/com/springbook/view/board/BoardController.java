c895484f7ee69c35cfb0af367d691bc8b48379f7
jihyeon <20181039@sungshin.ac.kr>
2021-02-07 20:53:12 +0900

    [DAY04] 어노테이션 기반 MVC - RequestParam, ModelAttribute, SessionAttributes

--- BoardProject/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardProject/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,14 +1,30 @@
 package com.springbook.view.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
+@SessionAttributes("board")
 public class BoardController {
+	
+	//검색 조건 목록 설정
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap(){
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
+	}
 	
 	// 글 등록
 	@RequestMapping("/insertBoard.do")
@@ -19,7 +35,13 @@ public class BoardController {
 	
 	// 글 수정
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+		System.out.println("번호 : " + vo.getSeq());
+		System.out.println("제목 : " + vo.getTitle());
+		System.out.println("작성자 : " + vo.getWriter());
+		System.out.println("내용 : " + vo.getContent());
+		System.out.println("등록일 : " + vo.getRegDate());
+		System.out.println("조회수 : " + vo.getCnt());
 		boardDAO.updateBoard(vo);
 		return "getBoardList.do";
 	}
@@ -40,7 +62,11 @@ public class BoardController {
 	
 	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
+	public String getBoardList(@RequestParam(value="searchCondition", defaultValue="TITLE", required=false) String condition,
+							   @RequestParam(value="searchKeyword", defaultValue="", required=false) String keyword,
+							   BoardVO vo, BoardDAO boardDAO, Model model) {
+		System.out.println("검색 조건 : " + condition);
+		System.out.println("검색 단어 : " + keyword);
 		model.addAttribute("boardList", boardDAO.getBoardList(vo));
 		return "getBoardList.jsp";
 	}	
